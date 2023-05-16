1f5a289aa6a399556e75d9d2ba6b8e095395751f
Tim Goalen <timgoalen@hotmail.com>
2023-05-16 09:34:44 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// get elements from html
+const playBtn = document.getElementById('play');
+const audio = document.getElementById('audio');
+
+// play audio with button
+playBtn.addEventListener('click', function() {
+    audio.play();
+})
+
+// audio files array
